8a8ac2562dc0f03413e11c865e6b452cbfd8bfdf
Harish Chandra Tanty <shanu14tnt@gmail.com>
2023-01-16 00:27:59 +0530

    updated heading color

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const div = document.querySelector("div");
 
 const h1 = document.getElementsByTagName("h1")[0]
-h1.style.color = "#2cb978"
+h1.style.color = "#3b5441"
 
 const p = document.createElement("p");
 const textNode = document.createTextNode("I have nothing to do today.");
