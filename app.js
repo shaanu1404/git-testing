106ed4baeda1f1977fb026bdab8bb87d952bb239
Harish Chandra Tanty <shanu14tnt@gmail.com>
2023-01-16 00:11:12 +0530

    Added vite

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const div = document.querySelector("div");
 
+const h1 = document.getElementsByTagName("h1")[0]
+h1.style.color = "#2cb978"
+
 const p = document.createElement("p");
 const textNode = document.createTextNode("I have nothing to do today.");
 p.textContent = textNode.textContent;
